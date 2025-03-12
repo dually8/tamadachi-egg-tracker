880fc01a161511a25c56f858cec5d4d8ce9bc5e1
CJ Coffey <cj@cjcoffey.com>
2025-03-12 16:55:25 -0400

    feat: add SQLite database support via prisma

---
+++ server/prisma/migrations/20250312201625_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "EggPrice" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "storeName" TEXT NOT NULL,
+    "storeLocation" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,30 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client';
 
-const app = new Hono()
+const prisma = new PrismaClient();
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.json({ message: 'Try hitting /egg-prices instead!' });
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get('/egg-prices', async (c) => {
+  const prices = await getEggPrices();
+  return c.json(prices);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
+
+async function getEggPrices() {
+  const prices = await prisma.eggPrice.findMany();
+  return prices;
+}
