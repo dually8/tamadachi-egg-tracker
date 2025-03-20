2274171ec9a432aae34793c5ff88bf7502daf3b1
CJ Coffey <cj@cjcoffey.com>
2025-03-20 17:03:44 -0400

    feat: add charts for price tracking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
+import PriceHistoryChart from '@/components/price-history-chart';
 import PriceTableAgGrid from '@/components/price-table-ag-grid';
 import { db } from '@/db/drizzle';
-import { priceTable } from '@/db/schema';
+import { Price, priceTable } from '@/db/schema';
 import { desc } from 'drizzle-orm';
 
 export default async function Home() {
   // Fetch data from the database
   const prices = await db.select().from(priceTable).orderBy(desc(priceTable.date));
+  const storeNames = Array.from(new Set(prices.map(price => price.storeName))).sort((a,b) => a.localeCompare(b));
+  const priceMap = new Map<string, Price[]>();
+  storeNames.forEach(storeName => {
+    priceMap.set(storeName, prices.filter(price => price.storeName === storeName));
+  });
   return (
     <main>
       <PriceTableAgGrid prices={prices} />
+      {storeNames.map(storeName => (
+        <PriceHistoryChart
+          key={storeName}
+          title={storeName}
+          data={priceMap.get(storeName) || []}
+        />
+      ))}
     </main>
   );
 }
---
+++ src/components/price-history-chart.tsx
@@ -0,0 +1,65 @@
+'use client';
+import { Price } from '@/db/schema';
+import { AgChartOptions } from 'ag-charts-community';
+import { AgCharts } from 'ag-charts-react';
+import { useTheme } from 'next-themes';
+import { useMemo } from 'react';
+
+// X axis is time
+// Y axis is price
+
+type PriceHistoryChartProps = {
+  title: string;
+  data: Price[];
+}
+export default function PriceHistoryChart(props: Readonly<PriceHistoryChartProps>) {
+  const { systemTheme } = useTheme();
+
+  const options = useMemo<AgChartOptions>(() => ({
+    theme: systemTheme === 'dark' ? 'ag-default-dark' : 'ag-default',
+    title: {
+      text: props.title,
+    },
+    data: props.data.map(item => ({
+      price: item.price,
+      date: new Date(item.date) // Ensure date is a Date object
+    })),
+    series: [{
+      type: 'line',
+      xKey: 'date',
+      yKey: 'price',
+      yName: 'Price',
+      label: {
+        enabled: true,
+        fontWeight: 'bold',
+      },
+    }],
+    axes: [
+      {
+        type: 'time',
+        position: 'bottom',
+        label: {
+          formatter: (params) => new Date(params.value).toLocaleDateString(),
+        },
+        title: {
+          text: 'Date',
+        },
+      },
+      {
+        type: 'number',
+        position: 'left',
+        label: {
+          formatter: (params) => `$${params.value.toFixed(2)}`,
+        },
+        title: {
+          text: 'Price',
+        },
+      }
+    ],
+  }), [props.title, props.data, systemTheme]);
+  return (
+    <div className='py-2'>
+      <AgCharts options={options} />
+    </div>
+  );
+}
